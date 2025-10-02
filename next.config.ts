9f41bf63c269f957c67039d693352edfeff1b69c
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-10-02 12:04:13 +0100

    chore: update with project font and style :fire:

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,37 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  redirects: async function () {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: false,
+      },
+    ]
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cpip-dev-public.s3.eu-west-1.amazonaws.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'numoniimages.s3.amazonaws.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cpip-dev-public.s3.eu-west-1.amazonaws.com',
+        port: '',
+        pathname: '**',
+      }
+    ],
+  }
 };
 
 export default nextConfig;
