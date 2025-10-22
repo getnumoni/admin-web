61f49de628c6f87338a59ee4d4d85ccef2bb88de
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-10-22 15:03:52 +0100

    chore: implement support page :fire:

---
+++ app/dashboard/support/page.tsx
@@ -0,0 +1,12 @@
+
+import Support from "@/components/admin/support";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Support",
+  description: "Support",
+}
+
+export default function Page() {
+  return <Support />;
+}
