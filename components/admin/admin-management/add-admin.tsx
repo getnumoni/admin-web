a4e9325aa993b79451dd540d297ff856d6b3e0ca
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-10-24 04:37:11 +0100

    chore: call the mutation hook :fire:

--- components/admin/admin-management/add-admin.tsx
+++ components/admin/admin-management/add-admin.tsx
@@ -2,15 +2,17 @@
 
 import { Button } from '@/components/ui/button';
 import { Form } from '@/components/ui/form';
+import { useCreateAdmin } from '@/hooks/mutation/useCreateAdmin';
 import { adminSchema, type AdminFormData } from '@/lib/schemas/admin-schema';
+import { CreateAdminPayload } from '@/lib/types';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import ProfileUploadSection from '../add-merchants/profile-upload-section';
 import BasicInformation from './basic-information';
 
 export default function AddAdmin() {
-  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const { handleCreateAdmin, isPending } = useCreateAdmin();
 
   const form = useForm<AdminFormData>({
     resolver: zodResolver(adminSchema),
@@ -20,6 +22,7 @@ export default function AddAdmin() {
       emailAddress: '',
       phoneNumber: '',
       roleType: '',
+      department: '',
       password: '',
       confirmPassword: '',
       notifyByEmail: false
@@ -33,22 +36,17 @@ export default function AddAdmin() {
   };
 
   const onSubmit = async (data: AdminFormData) => {
-    setIsSubmitting(true);
-
-    try {
-      // Log the payload to console
-      console.log('Admin Form Payload:', data);
-
-      // Here you would typically send the data to your API
-      // await createAdmin(data);
-
-      alert('Admin created successfully!');
-
-    } catch (error: unknown) {
-      console.error('Error creating admin:', error);
-    } finally {
-      setIsSubmitting(false);
+    const payload: CreateAdminPayload = {
+      name: data.fullName,
+      email: data.emailAddress,
+      phone: data.phoneNumber,
+      loginAccess: true,
+      roleName: data.roleType,
+      department: data.department,
+      password: data.password,
     }
+
+    handleCreateAdmin(payload);
   };
 
   return (
@@ -70,9 +68,9 @@ export default function AddAdmin() {
         <div className="p-6 flex justify-end">
           <Button
             type="submit"
-            disabled={isSubmitting}
+            disabled={isPending}
             className="bg-theme-dark-green hover:bg-theme-dark-green/90 text-white px-8 py-6 rounded-lg font-medium transition-colors disabled:opacity-50 disabled:cursor-not-allowed"
-            isLoading={isSubmitting}
+            isLoading={isPending}
             loadingText="Creating Admin..."
           >
             Create User
