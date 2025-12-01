e064d96d96e7ddaed91cc8db129ef237bebd6a7c
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-12-01 11:36:21 +0100

    chore: optional for verification status :fire:

--- hooks/mutation/useAddMerchantKyc.ts
+++ hooks/mutation/useAddMerchantKyc.ts
@@ -10,7 +10,7 @@ type AddMerchantKycPayload = {
   tinIdentificationNumber?: string;
   businessRegistrationNumber: string;
   documentUrl: string;
-  verificationStatus: boolean;
+  verificationStatus?: boolean;
 }
 
 
