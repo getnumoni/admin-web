11f58f06db9a731912468171099c16f88588c608
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2026-01-09 19:51:50 +0100

    feat: created a hook for making deal internal :rocket:

---
+++ hooks/mutation/useMakeDealInternal.ts
@@ -0,0 +1,34 @@
+import api from "@/lib/api";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+export type MakeDealInternalPayload = {
+  dealId: string;
+  isInternal: boolean;
+};
+
+export const useMakeDealInternal = () => {
+  const queryClient = useQueryClient();
+  const { mutate, isPending, isSuccess } = useMutation({
+    mutationFn: (data: MakeDealInternalPayload) => api.post("/admin/updateDealInternalStatus", data),
+    onSuccess: (data) => {
+      if (data) {
+        toast.success(data?.data?.message ?? "Deal made internal successfully");
+
+      }
+    },
+    onError: (error: { response: { data: { message: string } } }) => {
+      // console.log("Failed to create POS", error);
+      toast.error(error?.response?.data?.message ?? "Failed to make deal internal");
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["deals"] });
+    },
+  });
+
+  const handleMakeDealInternal = (data: MakeDealInternalPayload) => {
+    mutate(data);
+  };
+
+  return { handleMakeDealInternal, isPending, isSuccess };
+};
