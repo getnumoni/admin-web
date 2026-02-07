4dca7ba79070502f0b83df5039a3b9999089601d
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2026-02-07 06:50:45 +0100

    chore: add time options here :zap:

--- data/index.ts
+++ data/index.ts
@@ -334,6 +334,33 @@ const mockMerchantData = {
 };
 
 
+const timeOptions = [
+  { value: "6:00 AM", label: "6:00 AM" },
+  { value: "7:00 AM", label: "7:00 AM" },
+  { value: "8:00 AM", label: "8:00 AM" },
+  { value: "9:00 AM", label: "9:00 AM" },
+  { value: "10:00 AM", label: "10:00 AM" },
+  { value: "11:00 AM", label: "11:00 AM" },
+  { value: "12:00 PM", label: "12:00 PM" },
+  { value: "1:00 PM", label: "1:00 PM" },
+  { value: "2:00 PM", label: "2:00 PM" },
+  { value: "3:00 PM", label: "3:00 PM" },
+  { value: "4:00 PM", label: "4:00 PM" },
+  { value: "5:00 PM", label: "5:00 PM" },
+  { value: "6:00 PM", label: "6:00 PM" },
+  { value: "7:00 PM", label: "7:00 PM" },
+  { value: "8:00 PM", label: "8:00 PM" },
+  { value: "9:00 PM", label: "9:00 PM" },
+  { value: "10:00 PM", label: "10:00 PM" },
+  { value: "11:00 PM", label: "11:00 PM" },
+  { value: "12:00 AM", label: "12:00 AM" },
+  { value: "1:00 AM", label: "1:00 AM" },
+  { value: "2:00 AM", label: "2:00 AM" },
+  { value: "3:00 AM", label: "3:00 AM" },
+  { value: "4:00 AM", label: "4:00 AM" },
+  { value: "5:00 AM", label: "5:00 AM" },
+];
+
 
 export {
   adminNavigationItem, benefitsData,
@@ -345,8 +372,7 @@ export {
   socialMediaPlatforms,
   summaryData,
   tabs,
-  timelineOptions,
-  transactionData,
+  timelineOptions, timeOptions, transactionData,
   whatYouCanDoData,
   years
 };
